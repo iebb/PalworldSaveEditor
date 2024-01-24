a0943db41b2b08728045674e8eef0cfa6e8d2185
ieb <1@wa.vg>
2024-01-24 23:27:52 +0800

    fix

--- src/libs/save.js
+++ src/libs/save.js
@@ -14,59 +14,65 @@ export const analyzeFile = async (file) => {
       reader.onload = async (e) => {
         const serial = new Serializer(Buffer.from(reader.result));
 
-        const lenDecompressed = serial.readInt32();
-        const lenCompressed = serial.readInt32();
-        const magic = serial.read(3);
-        const saveType = serial.read(1);
+        try {
 
 
-        const data = serial.read(lenCompressed);
-        let decompressed = data;
+          const lenDecompressed = serial.readInt32();
+          const lenCompressed = serial.readInt32();
+          const magic = serial.read(3);
+          const saveType = serial.read(1);
 
-        switch (saveType[0]) {
-          case 0x32:
-            decompressed = pako.inflate(decompressed);
-          // eslint-disable-next-line no-fallthrough
-          case 0x31:
-            decompressed = pako.inflate(decompressed);
-            break;
+
+          const data = serial.read(lenCompressed);
+          let decompressed = data;
+
+          switch (saveType[0]) {
+            case 0x32:
+              decompressed = pako.inflate(decompressed);
+            // eslint-disable-next-line no-fallthrough
+            case 0x31:
+              decompressed = pako.inflate(decompressed);
+              break;
+          }
+
+
+          // saveAs(new Blob([decompressed], {type: "application/binary"}), "chunk0");
+
+          const typeMap = new Map();
+          typeMap.set(
+            ".worldSaveData.CharacterSaveParameterMap.Key", "Struct"
+          );
+          typeMap.set(
+            ".worldSaveData.FoliageGridSaveDataMap.Key", "Struct",
+          );
+          typeMap.set(
+            ".worldSaveData.FoliageGridSaveDataMap.ModelMap.InstanceDataMap.Key", "Struct",
+          );
+          typeMap.set(
+            ".worldSaveData.MapObjectSpawnerInStageSaveData.Key", "Struct",
+          );
+          typeMap.set(
+            ".worldSaveData.ItemContainerSaveData.Key", "Struct",
+          );
+          typeMap.set(
+            ".worldSaveData.CharacterContainerSaveData.Key", "Struct",
+          );
+
+          console.time("deserialize");
+          const gvas = JSON.parse(deserialize(decompressed, typeMap));
+          console.timeEnd("deserialize");
+
+          resolve({
+            fileName: file.name,
+            lenDecompressed,
+            lenCompressed,
+            magic,
+            saveType,
+            gvas
+          });
+        } catch (e) {
+          alert("Is it really a Palworld Save?");
         }
-
-
-        // saveAs(new Blob([decompressed], {type: "application/binary"}), "chunk0");
-
-        const typeMap = new Map();
-        typeMap.set(
-          ".worldSaveData.CharacterSaveParameterMap.Key", "Struct"
-        );
-        typeMap.set(
-          ".worldSaveData.FoliageGridSaveDataMap.Key", "Struct",
-        );
-        typeMap.set(
-          ".worldSaveData.FoliageGridSaveDataMap.ModelMap.InstanceDataMap.Key", "Struct",
-        );
-        typeMap.set(
-          ".worldSaveData.MapObjectSpawnerInStageSaveData.Key", "Struct",
-        );
-        typeMap.set(
-          ".worldSaveData.ItemContainerSaveData.Key", "Struct",
-        );
-        typeMap.set(
-          ".worldSaveData.CharacterContainerSaveData.Key", "Struct",
-        );
-
-        console.time("deserialize");
-        const gvas = JSON.parse(deserialize(decompressed, typeMap));
-        console.timeEnd("deserialize");
-
-        resolve({
-          fileName: file.name,
-          lenDecompressed,
-          lenCompressed,
-          magic,
-          saveType,
-          gvas
-        });
       };
       reader.readAsArrayBuffer(file);
     }
@@ -76,25 +82,29 @@ export const analyzeFile = async (file) => {
 
 export const writeFile = async ({ magic, saveType, gvas }, filename = "save.sav") => {
 
-  let serialized = serialize(JSON.stringify(gvas));
-  const lenDecompressed = serialized.length;
+  try {
+    let serialized = serialize(JSON.stringify(gvas));
+    const lenDecompressed = serialized.length;
 
-  switch (saveType[0]) {
-    case 0x32:
-      serialized = pako.deflate(serialized);
-    // eslint-disable-next-line no-fallthrough
-    case 0x31:
-      serialized = pako.deflate(serialized);
-      break;
+    switch (saveType[0]) {
+      case 0x32:
+        serialized = pako.deflate(serialized);
+      // eslint-disable-next-line no-fallthrough
+      case 0x31:
+        serialized = pako.deflate(serialized);
+        break;
+    }
+
+    const lenCompressed = serialized.length;
+    const buf = new Buffer(4 + 4 + 4 + lenCompressed);
+
+    buf.writeInt32LE(lenDecompressed);
+    buf.writeInt32LE(lenCompressed, 4);
+    buf.writeInt32LE(magic * 256 + saveType, 8);
+    buf.set(serialized, 12);
+    saveAs(new Blob([buf], {type: "application/binary"}), filename);
+  } catch (e) {
+    alert("Serialization failed. Have you accidentally removed something?");
   }
 
-  const lenCompressed = serialized.length;
-  const buf = new Buffer(4 + 4 + 4 + lenCompressed);
-
-  buf.writeInt32LE(lenDecompressed);
-  buf.writeInt32LE(lenCompressed, 4);
-  buf.writeInt32LE(magic * 256 + saveType, 8);
-  buf.set(serialized, 12);
-
-  saveAs(new Blob([buf], {type: "application/binary"}), filename);
 }
